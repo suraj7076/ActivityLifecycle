6f56a21834fd1e7a56fe74fb2bf1656f69892a2d
suraj7076 <surajmaity981@gmail.com>
2022-09-14 09:26:38 +0530

    Activity Lifecycle Updated

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.activitylifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("onCreate called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy called")
+    }
+}
